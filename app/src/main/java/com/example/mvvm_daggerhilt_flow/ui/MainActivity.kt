d5aa16abbc8da9e1764df24408c98758f6b6b842
furkan.kucuk <oiua66f7olp67z3wciqh7tqwi5lvok5342iw7zwrmgp5sacrur3a>
2022-10-18 11:59:36 +0300

    Add Search

--- app/src/main/java/com/example/mvvm_daggerhilt_flow/ui/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_daggerhilt_flow/ui/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.mvvm_daggerhilt_flow.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.mvvm_daggerhilt_flow.viewModel.MainViewModel
+import com.example.mvvm_daggerhilt_flow.Product
 import com.example.mvvm_daggerhilt_flow.data.NetworkResult
 import com.example.mvvm_daggerhilt_flow.databinding.ActivityMainBinding
+import com.example.mvvm_daggerhilt_flow.viewModel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,14 +22,36 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val mainViewModel: MainViewModel by viewModels()
     private val productAdapter: ProductAdapter = ProductAdapter()
+    private var productList = ArrayList<Product>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setupAdapter()
+        setupSearch()
         subscribeObserve()
+    }
+
+    private fun setupSearch() {
+        binding.etSearch.doOnTextChanged { inputText, _, _, _ ->
+            if (inputText != null) {
+                if (inputText.isEmpty()) {
+                    if (productList.size != 0)
+                        productAdapter.differ.submitList(productList)
+                } else {
+
+                    val currentTextLength = inputText.length
+                    if (currentTextLength >= 2) {
+                        Handler(Looper.getMainLooper()).postDelayed({
+                            val productFilterList =
+                                productList.filter { it.title.lowercase().contains(inputText) }
+                            productAdapter.differ.submitList(productFilterList)
+                        }, 500)
+                    }
+                }
+            }
+        }
     }
 
     private fun subscribeObserve() {
@@ -45,9 +71,11 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 is NetworkResult.Success -> {
+                    productList.clear()
                     hideLoading()
                     it.let { result ->
                         try {
+                            productList.addAll(result.data)
                             productAdapter.differ.submitList(result.data)
                         } catch (e: Throwable) {
                             Log.e("CATCH", e.message.toString())
